bdd64464329019a2adb1fb96c66267f1c130dca4
liwenguo123 <lguosky@gmail.com>
2016-05-16 00:33:05 -0400

    Update Account.java

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -2,6 +2,7 @@ package com.abc;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Date;
 
 public class Account {
 
@@ -9,11 +10,17 @@ public class Account {
     public static final int SAVINGS = 1;
     public static final int MAXI_SAVINGS = 2;
 
+    private final int accountNo;
     private final int accountType;
+    private int balance;
+	private Date latestWithdrawDate;
     public List<Transaction> transactions;
 
-    public Account(int accountType) {
+    public Account(int accountNo, int accountType) {
+        this.accountNo = accountNo;
         this.accountType = accountType;
+        this.balance = 0;
+		this.latestWithdrawDate = DateProvider.now();
         this.transactions = new ArrayList<Transaction>();
     }
 
@@ -22,6 +29,7 @@ public class Account {
             throw new IllegalArgumentException("amount must be greater than zero");
         } else {
             transactions.add(new Transaction(amount));
+			balance += amount;
         }
     }
 
@@ -30,6 +38,10 @@ public void withdraw(double amount) {
         throw new IllegalArgumentException("amount must be greater than zero");
     } else {
         transactions.add(new Transaction(-amount));
+		if (amount > balance)
+			throw new IllegalArgumentException("amount must be less than the balance");
+		balance -= amount;
+		latestWithdrawDate = DateProvider.now();
     }
 }
 
@@ -41,15 +53,13 @@ public void withdraw(double amount) {
                     return amount * 0.001;
                 else
                     return 1 + (amount-1000) * 0.002;
-//            case SUPER_SAVINGS:
-//                if (amount <= 4000)
-//                    return 20;
             case MAXI_SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.02;
-                if (amount <= 2000)
-                    return 20 + (amount-1000) * 0.05;
-                return 70 + (amount-2000) * 0.1;
+				Date diff = DateProvider.now() - latestWithdrawDate;
+				int numDays = (int)TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS));
+				if (numDays > 10):
+					return amount * 0.05
+				else:
+					return amount * 0.001
             default:
                 return amount * 0.001;
         }
@@ -70,4 +80,7 @@ public void withdraw(double amount) {
         return accountType;
     }
 
+	public getBalance() { 
+		return balance;
+	}		
 }
