082e3c043d510fa3315c37f4e7df0e4584da818c
liwenguo123 <lguosky@gmail.com>
2016-05-16 00:29:41 -0400

    Update BankTest.java

--- src/test/java/com/abc/BankTest.java
+++ src/test/java/com/abc/BankTest.java
@@ -8,17 +8,50 @@ public class BankTest {
     private static final double DOUBLE_DELTA = 1e-15;
 
     @Test
-    public void customerSummary() {
+    public void customerSummary(String name, ArrayList<int> accountNo， ArrayLIst<int> accountType) {
         Bank bank = new Bank();
-        Customer john = new Customer("John");
-        john.openAccount(new Account(Account.CHECKING));
-        bank.addCustomer(john);
+        Customer customer = new Customer(name);
+		int numAcc = accountNo.size();
+		for (int i = 0; i < numAcc; i++) {
+			customer.openAccount(new Account(accountNo[i], accountType[i]);
+        bank.addCustomer(customer);
 
-        assertEquals("Customer Summary\n - John (1 account)", bank.customerSummary());
+        assertEquals("Customer Summary\n - " + name + "(" numAcc.toString() + (numAcc == 1) ? "account)" : "accounts)", bank.customerSummary());
     }
 
     @Test
-    public void checkingAccount() {
+	public void customerSummary_call() {}
+		ArrayList<int> accNos = Stream.of(1234, 1235, 1236， 1237, null).collect(Collectors.toList());
+		ArrayList<int> accTypes = Stream.of(Account.CHECKING, Account.SAVINGS, Account.MAXI_SAVINGS, 10， null).collect(Collectors.toList());
+		//ArrayList<String> names = Stream.of("John", "John", "Jone", "Jone", "").collect(Collectors.toList());
+		// int[] accNos = new int[] {1234, 1235, 1236， 1237, null};
+		
+		String name = "john";
+		customerSummary(name, accNos, accTypes)；
+	}
+	
+    @Test
+	public void general_account(String name, Account account, ArrayList<double> depositAmounts, ArrayList<double> withdrawAmounts) { 
+        Bank bank = new Bank();
+        Customer customer = new Customer(name).openAccount(account)；
+		bank.addCustomer(customer);
+		for (double damount : depositAmounts)
+			account.deposit(damount); 
+		for (double wamount : withdrawAmounts)
+			account.withdraw(damount);
+		double interest = 0.0;
+		if (account.accountType == CHECKING)
+			interest = 0.1;	// need to recalculate case by case
+		else if (account.accountType == SAVINGS)
+			interest = 2.0;	// need to recalculate case by case
+		else if (account.accountType == MAXI_SAVINGS)
+			interest = 170.0;	// need to recalculate case by case
+		
+        assertEquals(interest, bank.totalInterestPaid(), DOUBLE_DELTA);
+	}
+	
+    @Test
+	public void checkingAccount() {
         Bank bank = new Bank();
         Account checkingAccount = new Account(Account.CHECKING);
         Customer bill = new Customer("Bill").openAccount(checkingAccount);
@@ -30,25 +63,16 @@ public class BankTest {
     }
 
     @Test
-    public void savings_account() {
-        Bank bank = new Bank();
-        Account checkingAccount = new Account(Account.SAVINGS);
-        bank.addCustomer(new Customer("Bill").openAccount(checkingAccount));
-
-        checkingAccount.deposit(1500.0);
-
-        assertEquals(2.0, bank.totalInterestPaid(), DOUBLE_DELTA);
-    }
-
-    @Test
-    public void maxi_savings_account() {
-        Bank bank = new Bank();
-        Account checkingAccount = new Account(Account.MAXI_SAVINGS);
-        bank.addCustomer(new Customer("Bill").openAccount(checkingAccount));
-
-        checkingAccount.deposit(3000.0);
-
-        assertEquals(170.0, bank.totalInterestPaid(), DOUBLE_DELTA);
-    }
-
+	public void general_account_call() {
+		String name = "John";
+		int anum = 12345;
+		ArrayList<int> atypes = Stream.of(Account.CHECKING, Account.SAVINGS, Account.MAXI_SAVINGS, 10， null).collect(Collectors.toList());
+		ArrayList<double> damount = Stream.of(100.0, 0.0, -30.0, 100.0).collect(Collectors.toList());
+		ArrayList<double> wamount = Stream.of(50.0, 0.0, -20.0, 1000.0).collect(Collectors.toList());
+		for (int atype : atypes) {
+			account = new Account(anum, atype);
+			general_account(name, account, damount, wamount);
+		}
+	}
+	
 }
