df12cd9f629f930705dd18f028f2cef8e09f4336
liwenguo123 <lguosky@gmail.com>
2016-05-16 00:34:05 -0400

    Update Customer.java

--- src/main/java/com/abc/Customer.java
+++ src/main/java/com/abc/Customer.java
@@ -23,6 +23,21 @@ public class Customer {
         return this;
     }
 
+	// Transfer amount from account1 to account2
+	public transferBetweenAccounts(double amount, Account account1, Account account2) {
+		account1.withdraw(amount);
+		account2.deposit(amount);		
+		return; 
+	}
+	
+	public double getTotalBalance()
+	{
+		double totalBalance = 0.0;
+		for (Account account : accounts)
+			totalBalance += account.getBalance();
+		return totalBalance
+	}
+	
     public int getNumberOfAccounts() {
         return accounts.size();
     }
@@ -37,12 +52,12 @@ public class Customer {
     public String getStatement() {
         String statement = null;
         statement = "Statement for " + name + "\n";
-        double total = 0.0;
+        //double total = 0.0;
         for (Account a : accounts) {
             statement += "\n" + statementForAccount(a) + "\n";
-            total += a.sumTransactions();
+            //total += a.getBalance();
         }
-        statement += "\nTotal In All Accounts " + toDollars(total);
+        statement += "\nTotal In All Accounts " + toDollars(getTotalBalance());
         return statement;
     }
 
@@ -66,6 +81,32 @@ public class Customer {
         double total = 0.0;
         for (Transaction t : a.transactions) {
             s += "  " + (t.amount < 0 ? "withdrawal" : "deposit") + " " + toDollars(t.amount) + "\n";
+            total += t.amount;
+        }
+        s += "Total " + toDollars(total);
+        return s;
+    }
+
+    private String statementWithDateForAccount(Account a) {
+        String s = "";
+
+       //Translate to pretty account type
+        switch(a.getAccountType()){
+            case Account.CHECKING:
+                s += "Checking Account\n";
+                break;
+            case Account.SAVINGS:
+                s += "Savings Account\n";
+                break;
+            case Account.MAXI_SAVINGS:
+                s += "Maxi Savings Account\n";
+                break;
+        }
+
+        //Now total up all the transactions
+        double total = 0.0;
+        for (Transaction t : a.transactions) {
+            s += "  " + (t.amount < 0 ? "withdrawal" : "deposit") + " " + toDollars(t.amount) + " " + t.transactionDate.toString() + "\n";
             total += t.amount;
         }
         s += "Total " + toDollars(total);
