b7f2e135e0643fdfb4b54fdfd6aa398ad33693a5
liwenguo123 <lguosky@gmail.com>
2016-05-16 00:31:14 -0400

    Update CustomerTest.java

--- src/test/java/com/abc/CustomerTest.java
+++ src/test/java/com/abc/CustomerTest.java
@@ -33,25 +33,77 @@ public class CustomerTest {
                 "Total In All Accounts $3,900.00", henry.getStatement());
     }
 
+    @Test //Test customer statement generation
+	public void testApp(String name, ArrayList<Account> accounts, ArrayList<ArrayList<double>> amounts) {
+		Customer customer = new Customer(name);
+		String stmt = "";
+		int i = 0;
+		for (Account account : accounts) {
+			customer.openAccount(account);
+			String atype="";
+			if (account.accountType == Account.CHECKING)
+				atype = "Checking";
+			else if (account.accountType == Account.SAVINGS)
+				atype = "Saving";
+			else if (account.accountType == Account.MAXI_SAVINGS)
+				atype = "Maxi Savings";
+			stmt += "\n\n“ + atype + " Account\n";
+			for (ArrayList<double> accountAmounts : amounts[i]) {
+				for (double amount : accountAmounts) {
+					if (amount > 0) {
+						account.deposit(amount);
+						stmt += " deposit " + _toDollars(abs(amount));
+					}
+					else if (amount < 0) {
+						account.withdraw(-amount);
+						stmt += " withdraw " + _toDollars(abs(amount));
+					}
+				}
+			}
+			stmt += "\nTotal" + _toDollars(abs(account.balance));
+			i++;
+		}
+		stmt += "\n\nTotal In All Accounts " + _toDollars(customer.getTotalBalance());
+		
+		assert_equals(customer.getStatement(), stmt); 
+	}
+	
     @Test
-    public void testOneAccount(){
-        Customer oscar = new Customer("Oscar").openAccount(new Account(Account.SAVINGS));
-        assertEquals(1, oscar.getNumberOfAccounts());
-    }
+	public void testApp_call() {
+		String name = "Henry";
+		int anum = 12345;
+		List<Account> accounts = new ArrayList<Account>();
+		ArrayList<int> atypes = Stream.of(Account.CHECKING, Account.SAVINGS, Account.MAXI_SAVINGS).collect(Collectors.toList());
+		for (int atype : atypes) {
+			Account account = new Account(anum, atype);
+			accounts.add(account);
+		}
+		ArrayList<double> camount = Stream.of(100.0, -50.0, -30.0).collect(Collectors.toList());
+		ArrayList<double> samount = Stream.of(100.0, -50.0, 10.0).collect(Collectors.toList());
+		ArrayList<double> mamount = Stream.of(1000.0, -500.0, 2000.0).collect(Collectors.toList());
+		ArrayList<ArrayList<double>> amounts = Stream.of(camount, samount, mamount).collect(Collectors.toList());
+		testApp(name, accounts, amounts);
+	}
 
     @Test
-    public void testTwoAccount(){
-        Customer oscar = new Customer("Oscar")
-                .openAccount(new Account(Account.SAVINGS));
-        oscar.openAccount(new Account(Account.CHECKING));
-        assertEquals(2, oscar.getNumberOfAccounts());
-    }
-
-    @Ignore
-    public void testThreeAcounts() {
-        Customer oscar = new Customer("Oscar")
-                .openAccount(new Account(Account.SAVINGS));
-        oscar.openAccount(new Account(Account.CHECKING));
-        assertEquals(3, oscar.getNumberOfAccounts());
-    }
+	public void testMultipleAccount(String customerName, ArrayList<Account> accounts) {
+		customer = new Customer(customerName);
+		for (Account account : accounts)
+			customer.openAccount(account); 
+		assert_equals(customer.numAccs(), accounts.size()); 
+	}
+	
+    @Test
+	public void testMultipleAccount_call() { 
+		String name = "John";
+		int anum = 12345;
+		List<Account> accounts = new ArrayList();
+		ArrayList<int> atypes = Stream.of(Account.CHECKING, Account.SAVINGS, Account.MAXI_SAVINGS).collect(Collectors.toList());
+		for (int atype : atypes) {
+			Account account = new Account(anum, atype);
+			accounts.add(account);
+		}
+		testMultipleAccount(name, accounts);
+	}
+	
 }
